d110684b4ed81539be0b1b7ee79fa4d68649b9f5
jcfkbacksDev <jcfkbacks@gmail.com>
2017-04-11 20:03:06 -0500

    Module 5 video 7

---
+++ Code/BlogEngine/BlogEngine.DataAccess/BlogEngineContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogEngine.Model;
+
+namespace BlogEngine.DataAccess
+{
+    public class BlogEngineContext : DbContext
+    {
+        public BlogEngineContext() : base("name=BlogEngineContext")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Publication> Publications { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Code/BlogEngine/Test.EntityFramework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogEngine.DataAccess;
+
+namespace Test.EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BlogEngineContext())
+            {
+                var listaBlogs = db.Blogs.ToList();
+            }
+        }
+    }
+}
