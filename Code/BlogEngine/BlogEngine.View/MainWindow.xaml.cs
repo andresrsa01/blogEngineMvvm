4eb47993af73b30a00e3aadd95b59e4bc023bfc2
jcfkbacksDev <jcfkbacks@gmail.com>
2017-04-13 13:43:39 -0500

    Add RelayCommand Class Implement Command Pattern Update Command's call in view with binding Initialize MainWindow View with the datacontext pointing to himself

--- Code/BlogEngine/BlogEngine.View/MainWindow.xaml.cs
+++ Code/BlogEngine/BlogEngine.View/MainWindow.xaml.cs
@@ -1,15 +1,76 @@
-﻿using System.Windows.Controls.Ribbon;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls.Ribbon;
+using BlogEngine.Common.Command;
+using BlogEngine.View.Annotations;
 
 namespace BlogEngine.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow :RibbonWindow
+    public partial class MainWindow : RibbonWindow, INotifyPropertyChanged
     {
+        private string _page;
+        public string Page
+        {
+            get
+            {
+                return _page;
+            }
+            set
+            {
+                _page = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        private RelayCommand _createBlogCommand;
+        public RelayCommand CreateBlogCommand
+        {
+            get
+            {
+                return _createBlogCommand ??
+                       (_createBlogCommand = new RelayCommand(param => this.OpenCreateBlog(), parama => true));
+            }
+
+        }
+
+        private RelayCommand _listBlogCommand;
+
+        public RelayCommand ListBlogCommand
+        {
+            get
+            {
+                return _listBlogCommand ??
+                       (_listBlogCommand = new RelayCommand(param => this.ListBlogs(), parama => true));
+            }
+        }
+
+        private void ListBlogs()
+        {
+            Page = "ListBlogs.xaml";
+        }
+
+
+        private void OpenCreateBlog()
+        {
+            Page = "CreateBlog.xaml";
+        }
+
         public MainWindow()
         {
-          
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
