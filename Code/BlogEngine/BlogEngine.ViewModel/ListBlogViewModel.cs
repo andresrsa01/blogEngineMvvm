3a01fda03b2cb5fdcad8aa211f788424aad8f68c
jcfkbacksDev <jcfkbacks@gmail.com>
2017-04-13 19:43:32 -0500

    Add ListBlogViewModel Update ListBlogs view, bindin to ListBlogViewModel

---
+++ Code/BlogEngine/BlogEngine.ViewModel/ListBlogViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogEngine.BusinessLogic;
+using BlogEngine.Common.Base;
+using BlogEngine.Model;
+
+namespace BlogEngine.ViewModel
+{
+    public class ListBlogViewModel : ViewModelBase
+    {
+        private ObservableCollection<Blog> _listBlogs;
+
+        public ObservableCollection<Blog> ListBlogs
+        {
+            get
+            {
+                using (var bl = new BLBlog())
+                {
+                    _listBlogs = new ObservableCollection<Blog>(bl.ListBlogs());
+                }
+                return _listBlogs;
+            }
+            set
+            {
+                _listBlogs = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
